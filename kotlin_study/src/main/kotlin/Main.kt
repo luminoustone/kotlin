63bb0ae0ff5747843ba68caf4dee8b634e7c8846
HyeonJung Lee <hj.lee@wafour.com>
2022-06-26 15:16:03 +0900

    init commit

---
+++ kotlin_study/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+data class Person(val name: String,
+                  val age: Int? = null)
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
