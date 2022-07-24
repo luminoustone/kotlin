3b65c41c92b095a1c6a22e6418fb76a708b4355d
HyeonJung Lee <hj.lee@wafour.com>
2022-07-25 08:30:40 +0900

    chapter 3
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25chapter 3
    
    07/25

---
+++ kotlin_study/src/main/kotlin/parsePath.kt
@@ -0,0 +1,22 @@
+fun parsePath(path:String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
+
+fun parsePathWithKoRegex(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, fileName, extension) = matchResult.destructured
+        println("Dir: $directory, name: $fileName, ext: $extension")
+    }
+}
+
+fun  main(args: Array<String>){
+    parsePath("/Users/june/kotlin-book/chapter.adoc")
+    parsePathWithKoRegex("/Users/june/kotlin-book/chapter.adoc")
+}
